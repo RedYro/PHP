c74e315c8f151a029540bcd611268b3337d80f95
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-04-24 16:51:29 +0200

    Updates PHP cours

--- index.php
+++ index.php
@@ -39,8 +39,36 @@
         </section>
     </header>
     <!-- fin header -->
-    <main>
-
+    <main class="container-fluid px-5">
+        <section class="row mt-5">
+            <h2 class="text-center ">1 - Pourquoi apprendre php</h2>
+            <div class="col-12 col-md-8 mt-3">
+                <ul> 
+                    <li>C'est un langage open-source. Cela signifie que PHP est librement disponible pour être utilisé et implémenté.</li>
+                    <li>Il est indépendant de la plate-forme, ce qui signifie que les applications développées à l'aide de PHP peuvent s'exécuter dans n'importe quel environnement.</li>
+                    <li>Il a une grande communauté de développeurs. La programmation consiste à aider et à être aidé ; par conséquent, une grande communauté signifierait plus d'aide.</li>
+                    <li>Il est régulièrement mis à jour et fonctionne donc bien avec les dernières technologies.</li>
+                    <li>Grâce au PHP, nous pouvons créer tout type de site : moteur de recherche de type Google, réseaux sociaux comme Facebook, plateforme multimédia comme YouTube ou encore site d'information comme Wikipedia, nous pouvons aussi développer des site d'e-commerce, des espaces membres, des pages d'administration, etc.</li>
+                    <li>Il est hautement compatible car il peut être intégré à plusieurs langages de programmation tels que HTML, Javascript et prend en charge différentes bases de données telles que MySQL, PostgreSQL, Oracle, etc.</li>
+                    <li>Le PHP nous sert en fait à créer un site web dynamique. Un site web dynamique va être un site dont les informations proviennent d'une BDD et pour cela on va apprendre à faire fonctionner le PHP et le SQL main dans la main.</li>
+                </ul>
+            </div>
+                <div class="col-12 col-md-4 d-flex justify-content-center">
+                    <img src="assets/img/PHP.png" alt="" width="250" >
+                </div>
+            <div class="col-10 m-auto">
+                <p class="alert alert-danger">
+                    Attention, il ne faut pas confondre un site web dynamique avec un site web d'animation géré par des écouteurs d'évènements comme peut le faire le JS.
+                </p>
+                <div class="alert alert-secondary">
+                    <p>Nous utiliserons à partir de maintenant l'appellation de <em>site dynamique</em> pour les sites dont les informations sont stockées en BDD et qui possèdent deux interfaces distinctes : </p>
+                    <ol>
+                        <li>Une interface "front" sur laquelle on retrouvera l'affichage "normal" du site, le côté pour les admins et les clients.</li>
+                        <li>Une interface "back" qui permettra d'assurer la gestion de la BDD et certains réglages de l'interface front => on l'appellera le back office ou la page d'administration.</li>
+                    </ol>
+                </div>
+            </div>
+        </section>
     </main>
     <footer>
 

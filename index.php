4ea19e36aafb374dd54d6e34fe4d4e315d7b10ae
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-04-24 16:41:00 +0200

    Updates PHP cours

--- index.php
+++ index.php
@@ -13,14 +13,13 @@
     <link rel="shortcut icon" type="image/x-icon" href="assets/img/logo.png">
 </head>
 <body>
-    <header>
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" alt="Logo PHP"></a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+    <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" alt="Logo PHP"></a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav me-auto mb-2 mb-lg-0">
                     <li class="nav-item">
                         <a class="nav-link" aria-current="page" href="index.php">Introduction</a>
@@ -29,10 +28,17 @@
                         <a class="nav-link" href="#"></a>
                     </li>
                 </ul>
-                </div>
             </div>
-        </nav>
+        </div>
+    </nav>
+    <!-- fin navbar -->
+    <header class="p-5 m-4 bg-light rounded-3 border ">
+        <section class="container py-5">
+            <h1 class="display-5 fw-bold">Introduction au PHP</h1>
+            <p class="col-md-12 fs-4"> PHP, ce sigle est un acronyme récursif  <span>Préprocesseur Hypertexte PHP</span> (PHP Hypertext Preprocessor). Il s'agit d'un langage de script côté serveur open source utilisé pour le développement Web dynamique et peut être intégré dans des codes HTML, notez bien qu'un navigateur ne comprend pas le PHP.</p> 
+        </section>
     </header>
+    <!-- fin header -->
     <main>
 
     </main>

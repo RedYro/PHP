4001365523b63a473943c801d2100a304e0aeb96
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-04-24 16:34:49 +0200

    Updates PHP cours

--- index.php
+++ index.php
@@ -14,7 +14,24 @@
 </head>
 <body>
     <header>
-        
+        <nav class="navbar navbar-dark bg-dark navbar-expand-lg">
+            <div class="container-fluid">
+                <a class="navbar-brand" href="index.php"><img src="assets/img/logo.png" alt="Logo PHP"></a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link" aria-current="page" href="index.php">Introduction</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#"></a>
+                    </li>
+                </ul>
+                </div>
+            </div>
+        </nav>
     </header>
     <main>
 

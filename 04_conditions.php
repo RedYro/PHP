ba61750e35d7377647d9403beb46c7eb9c341ba2
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-04-28 12:16:44 +0200

    Updates PHP cours

--- 04_conditions.php
+++ 04_conditions.php
@@ -95,11 +95,93 @@
             <?php
                 if ($a === 8){ // Si la condition est vraie alors on exécute le code suivant
                     echo "<p class=\"alert alert-danger\">\"a\" est égal à 8.</p>";
-                } else if($a != 10){ // Sinon si, on exécute celui-ci
+                } else if($a !== 10){ // Sinon si, on exécute celui-ci
                     echo "<p class=\"alert alert-danger\">\"a\" est différent de $a.</p>";
                 } else{ // Sinon on exécute celui-ci
                     echo "<p class=\"alert alert-success\">\"a\" est égal à $a.</p>";
                 }
+            ?>
+            <h2>Conditions ternaire</h2>
+            <?php
+                // la condtion ternaire est une autre syntaxe pour écrire un "if", "else"
+                echo ($a === 10) ? "<p class=\"alert alert-success\">\"a\" est égal à $a</p>" : "<p class=\"alert alert-danger\">\"a\" est différent de 10</p>"; // Dans la ternaire le "?" remplace le "if" et le ":" remplace le "else".
+            ?>
+            <h2>Opérateurs "==", "==="</h2>
+            <p>L'opérateur "<span>==</span>" permet de comparer une valeur égalité de valeur, alors que l'opérateur "<span>===</span>" permet de comparer de façon stricte (égalité de valeur et de type)</p>
+            <?php
+                $varA = 1; // INT
+                $varB = '1'; // STRING
+                // == 
+                if ($varA == $varB){ // Condition vraie car 1 et '1' sont équivalents
+                    echo "<p class=\"alert alert-success\">\"varA\" et \"varB\" sont de même valeur.</p>";
+                } else{
+                    echo "<p class=\"alert alert-danger\">\"varA\" et \"varB\" ne sont pas de même valeur.</p>";
+                }
+                // ===
+                if ($varA === $varB){ // Condition fausse car 1 et '1' sont équivalents mais ne sont pas du même type
+                    echo "<p class=\"alert alert-success\">\"varA\" et \"varB\" sont de même valeur.</p>";
+                } else{
+                    echo "<p class=\"alert alert-danger\">\"varA\" et \"varB\" ne sont pas de même type.</p>";
+                }
+            ?>
+            <h2>Condition avec opérateur combiné "<=>"</h2>
+            <?php
+                // $a = 11; $b = 5
+                echo "<p class=\"alert alert-primary\">".'($a <=> $b) = '.($a <=> $b)."</p>"; // Affiche 1
+                // $b = 11
+                $b = 10;
+                echo "<p class=\"alert alert-primary\">".'($a <=> $b) = '.($a <=> $b)."</p>"; // Affiche 0
+                // $b = 12
+                $b = 12;
+                echo "<p class=\"alert alert-primary\">".'($a <=> $b) = '.($a <=> $b)."</p>"; // Affiche -1
+                /* 
+                * Ici l'opérateur combiné compare à la fois le "<", "=" et ">" en retournant respectivement la valeur 1, 0 et -1
+                * "<" ==> -1 si a < b 
+                * "=" ==> 0 si a = b
+                * ">" ==> 1 si a > b 
+                */
+                $a = 50;
+                $b = 29;
+                if(($a <=> $b) == 1){
+                    echo "<p class=\"alert alert-primary\">\"a\" est supérieur à \"b\". ".'($a <=> $b) = '.($a <=> $b)."</p>";
+                } else if(($a <=> $b) == 0){
+                    echo "<p class=\"alert alert-primary\">\"a\" est égal à \"b\". ".'($a <=> $b) = '.($a <=> $b)."</p>";
+                } else if(($a <=> $b) == -1){
+                    echo "<p class=\"alert alert-primary\">\"a\" est inférieur à \"b\". ".'($a <=> $b) = '.($a <=> $b)."</p>";
+                }
+            ?>
+            <h2>Condition avec "switch"</h2>
+            <?php 
+                // La condition switch est une autre syntaxe pour écrire un if else if else quand on veut comparer une variable à une multitude de valeurs 
+                $langue = 'chinois';
+                switch ($langue) {
+                    case 'français':
+                        echo "<p class=\"alert alert-primary\">Bonjour ! (langue = $langue)</p>";
+                        break;
+                    case 'italien':
+                        echo "<p class=\"alert alert-primary\">Ciao ! (langue = $langue)</p>";
+                        break;
+                    case 'espagnol':
+                        echo "<p class=\"alert alert-primary\">Hola ! (langue = $langue)</p>";
+                        break;
+                    default:
+                        echo "<p class=\"alert alert-primary\">Nǐ hǎo ! (langue = $langue)</p>";
+                        break;
+                }
+                // switch avec l'opérateur de combinaison
+
+            //     switch ($a <=> $b) {
+            //         case -1 :  
+            //             echo 'a est plus petit que b';
+            //         break; // "break" est obligatoire pour quitter le witcgh une fois un "case " est exécuté
+            //          case 0 :
+            //         echo 'a est égal à b' ;
+            //         break;
+            //         case 1 :
+            //         echo 'a est plus grand que b';
+            //         break;
+                
+            //   }
             ?>
         </div>
     </main>

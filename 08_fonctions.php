b6f639a4668465ce557177e6d11562cb3b02f14c
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-05-09 17:03:24 +0200

    Updates PHP cours

--- 08_fonctions.php
+++ 08_fonctions.php
@@ -39,11 +39,12 @@
             </div>
             <?php
                 $myString = "Bonjour j'adore le PHP !";
+                echo "$myString <br>";
                 // Affiche d'un caractère d'une chaîne de caractères
                 echo $myString[3] . '<br>'; // Affiche la lettre "j"
                 // Modifier un caractère d'une chaîne de caractères
                 $myString[0] = "b";
-                echo $myString[0] . '<br>'; // Changement de la lettre "B" majuscule en "b" miniscule
+                echo $myString[0] . '<br>'; // Changement de la lettre "B" majuscule en "b" minuscule
                 // Extraire une partie d'une chaîne de caractères
                 $newString = substr($myString, 0, 9); // Cette fonction prend en paramètres la variable, le point de départ et la longueur que l'on souhaite obtenir
                 echo "$newString <br>"; 
@@ -54,7 +55,52 @@
                 Ex a voluptatum corporis repudiandae neque molestiae consequatur in ratione dicta ad laudantium, officiis quisquam velit cum ab assumenda officia fugiat tenetur, soluta dignissimos corrupti natus voluptates. Blanditiis, sapiente numquam.
                 Blanditiis minus illum magni ad architecto ex pariatur mollitia eos iusto neque necessitatibus, officiis voluptas consequatur possimus. Inventore ipsam nam voluptas, quasi dolores harum ullam, consequuntur, illum quas mollitia ab?";
                 $newText = substr($texte, 0, 40);
-                echo $newText;
+                // $newText2 = "...<a href=\"#\">Lire la suite</a>";
+                echo "<p>$newText...<a href=\"#\">Lire la suite</a></p>";
+                // /!\ Attention les espaces sont comptés comme des caractères dans la chaîne
+                // $newText3 = substr_replace($texte, '...<a href="#">Lire la suite</a>', 0, 40);
+                // echo $newText3
+
+                // Récupérer la position d'un caratère dans une chaîne de caractères 
+                echo "La position de la lettre \"H\" dans la phrase est : " . strpos($myString, "H") ."<br>"; // Position H = 19
+                // /!\ Attention la fonction est case sensitive, elle fait attention à la casse des lettres : si l'on met la lettre "h" en minuscule rien ne s'affichera
+                // var_dump(strpos($myString,"H"));
+                
+                // Récupérer la taille d'une chaîne de caratères
+                echo strlen($myString) . "<br>";
+
+                // Remplacer une partie de la chaîne
+                $myString = str_replace('PHP', 'JS', $myString); // Paremètres de la fonction : chaîne de caratères à changer, chaîne de caratères de remplacement et la variable à manipuler
+                echo "$myString <br>";
+                // Ici aussi la fonction est sensible à la casse, on ne change pas la valeur si il y a une différence entre la valeur cherchée et stockée
+                // Il existe une autre fonction qui ne fait pas de distinction entre les lettres majuscules et minuscules dans les chaîne de caractères
+                $myString = str_ireplace('BONJOUR', 'Hello', $myString);
+                echo "$myString <br>"; 
+
+                // Vérifier si la chaîne contient un mot particulier 
+                var_dump(str_contains($myString, 'JS')); // Paramètres : variable contenant la chaîne, mot à vérifier dans la variable
+                // str_contains() permet de vérifier si la chaîne contient un mot en particulier, cependant elle est sensible à la casse
+                // résultat en boolean "true" ou "false"
+                echo "<br>";
+
+                // Vérifier si la chaîne commence par ce que l'on passe dans les paramètres
+                var_dump(str_starts_with($myString, 'Hel')); // sensible à la casse // Permet de vérifier si la chaîne commence par un mot insérer en 2ème paramètre
+                echo "<br>";
+
+                // Vérifier si la chaîne se termine par ce que vous lui passez dans les paramètres 
+                var_dump(str_ends_with($myString, '!')); // sensible à la casse // Permet de vérifier si la chaîne se termine par un mot insérer en 2ème paramètre
+                echo "<br>";
+
+                // Supprimer les espaces en début et fin de chaîne
+                $testString = "!        Une phrase beaucoup trop longue avec des espaces inutiles au début et à la fin      !";
+                echo "$testString <br>";
+                echo strlen($testString) . " : Nombre de caractères <br>";
+
+                $newTrim = trim($testString);
+                echo "$newTrim <br>";
+                echo strlen($newTrim) . " : Nombre de caractères <br>";
+                $newTrim = trim($testString, '!');
+                echo "$newTrim <br>";
 
             ?>
         </div>

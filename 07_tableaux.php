e1400031bd3c05544416317abf32349a37714ed9
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-05-09 10:17:57 +0200

    Updates PHP cours

--- 07_tableaux.php
+++ 07_tableaux.php
@@ -3,4 +3,5 @@
     $titleH1 = "Tableaux en PHP";
     $paragraphe = "Hello World !";
     require_once ("inc/header.inc.php");
+    require_once ("inc/footer.inc.php");
 ?>
--- 08_fonctions.php
+++ 08_fonctions.php
@@ -3,4 +3,5 @@
     $titleH1 = "Fonctions en PHP";
     $paragraphe = "Hello World !";
     require_once ("inc/header.inc.php");
+    require_once ("inc/footer.inc.php");
 ?>

8ffbf194a87822f50e6e7cc73447a7d973fe153d
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-05-23 10:11:56 +0200

    Updates PHP cours

--- 03_site_cinema/inc/functions.inc.php
+++ 03_site_cinema/inc/functions.inc.php
@@ -23,10 +23,20 @@
                 $pdo = new PDO($dsn, DBUSER, DBPASSWORD);
                 $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
                 $pdo->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
-                echo "<p>Connexion avec succès</p>";
+                // echo "<p>Connexion avec succès</p>";
             } catch(PDOException $e){
                 die($e->getMessage());
             }
+            return $pdo; // Utilisation de "return" pour récupérer l'objet de la fonction afin de l'appeler en dehors de la fonction
         }
     connectionDB();
+
+    //------ Tables ------//
+    //------ Fonctions création de tables ------//
+    function creationTableCategories(){
+        $pdo = connectionDB();
+        $sql = "CREATE TABLE IF NOT EXISTS categories (id_category INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(50) NOT NULL, description TEXT NULL)";
+        $request = $pdo->exec($sql);
+    }
+    creationTableCategories();
 ?>

4e451bf5dff7dd3b6246cc7cc2d4cb186cab0ed1
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-05-23 14:21:01 +0200

    Updates PHP cours

---
+++ 03_site_cinema/admin/categories.php
@@ -0,0 +1,5 @@
+<?php
+    $title = "Categories";
+    require_once("../inc/functions.inc.php");
+    require_once("../inc/header.inc.php");
+?>
---
+++ 03_site_cinema/admin/users.php
@@ -0,0 +1,5 @@
+<?php
+    $title = "Users";
+    require_once("../inc/functions.inc.php");
+    require_once("../inc/header.inc.php");
+?>

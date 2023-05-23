2a0c3103af49323a38615498009c1909c6db22aa
LYFOUNG Grégory <125985512+RedYro@users.noreply.github.com>
2023-05-23 14:19:45 +0200

    Updates PHP cours

--- 03_site_cinema/admin/dashboard.php
+++ 03_site_cinema/admin/dashboard.php
@@ -7,7 +7,21 @@
     <div class="row">
         <div class="col-sm-6 col-md-4 col-lg-2">
             <div class="d-flex flex-column p-3 text-bg-dark sidebarre">
-                
+                <hr>
+                <ul class="nav nav-pills flex-column mb-auto">
+                    <li>
+                        <a href="" class="nav-link text-light">Back-office</a>
+                    </li>
+                    <li>
+                        <a href="" class="nav-link text-light">Films</a>
+                    </li>
+                    <li>
+                        <a href="" class="nav-link text-light">Catégories</a>
+                    </li>
+                    <li>
+                        <a href="" class="nav-link text-light">Utilisateurs</a>
+                    </li>
+                </ul>
             </div>
         </div>
     </div>
